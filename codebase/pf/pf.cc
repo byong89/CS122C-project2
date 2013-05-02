876f0b1fe118b755fbf6903b1b4b0d0997f822a0
byong89 <bsy0289@gmail.com>
2013-05-01 22:27:44 -0700

    Added Page File

--- codebase/pf/pf.cc
+++ codebase/pf/pf.cc
@@ -1,55 +1,65 @@
 #include "pf.h"
+#include "stdio.h"
 
 PF_Manager* PF_Manager::_pf_manager = 0;
 
 
-PF_Manager* PF_Manager::Instance(int cacheNumPages)
+PF_Manager* PF_Manager::Instance()
 {
     if(!_pf_manager)
-        _pf_manager = new PF_Manager(cacheNumPages);
-
-    return _pf_manager;
+        _pf_manager = new PF_Manager();
+    
+    return _pf_manager;    
 }
 
-
-PF_Manager::PF_Manager(int cacheNumPages)
+//Constructor
+PF_Manager::PF_Manager()
 {
 }
 
-
+//Destructor
 PF_Manager::~PF_Manager()
 {
 }
 
-
+    
 RC PF_Manager::CreateFile(const char *fileName)
 {
+
+	if(FILE* myFile = fopen(fileName, "w")) {
+		fclose(myFile);
+		return 0;
+	}
     return -1;
 }
 
 
 RC PF_Manager::DestroyFile(const char *fileName)
 {
-    return -1;
+	remove(fileName);
+	return 0;
 }
 
 
 RC PF_Manager::OpenFile(const char *fileName, PF_FileHandle &fileHandle)
 {
-    return -1;
+	FILE* tmp = fopen(fileName, "r+");
+    return fileHandle.setFile(tmp);
 }
 
 
 RC PF_Manager::CloseFile(PF_FileHandle &fileHandle)
 {
-    return -1;
+    return fileHandle.closeFile();
 }
 
 
 PF_FileHandle::PF_FileHandle()
 {
+	this->myFile = NULL;
+	this->pageCount = 0;
 }
-
+ 
 
 PF_FileHandle::~PF_FileHandle()
 {
@@ -58,25 +68,55 @@ PF_FileHandle::~PF_FileHandle()
 
 RC PF_FileHandle::ReadPage(PageNum pageNum, void *data)
 {
-    return -1;
+	fseek(this->myFile, PF_PAGE_SIZE * pageNum, SEEK_SET);
+	for(unsigned i = 0; i < PF_PAGE_SIZE; i++){
+        *((char *)data+i) = fgetc(this->myFile);
+    }
+	fseek(this->myFile, 0, SEEK_END);
+    return 0;
 }
 
 
 RC PF_FileHandle::WritePage(PageNum pageNum, const void *data)
 {
-    return -1;
+	if (pageNum > pageCount) {
+		AppendPage(data);
+	}
+	fseek(this->myFile, PF_PAGE_SIZE * pageNum, SEEK_SET);
+	fwrite(data, 1, PF_PAGE_SIZE, this->myFile);
+	fseek(this->myFile, 0, SEEK_END);
+
+    return 0;
 }
 
 
 RC PF_FileHandle::AppendPage(const void *data)
 {
-    return -1;
+	WritePage(this->pageCount, data);
+	this->pageCount++;
+
+    return 0;
 }
 
 
 unsigned PF_FileHandle::GetNumberOfPages()
 {
-    return -1;
+    return this->pageCount;
 }
 
+RC PF_FileHandle::setFile(FILE* &myFile){
+	if(this->myFile != NULL){
+		return -1;
+	}
+	this->myFile = myFile;
+	return 0;
+}
+
+RC PF_FileHandle::closeFile(){
+	if(fclose(this->myFile) == 0) {
+		this->myFile = NULL;
+		return 0;
+	}
+	return -1;
+}
 
--- codebase/pf/pf.h
+++ codebase/pf/pf.h
@@ -1,6 +1,10 @@
 #ifndef _pf_h_
 #define _pf_h_
 
+#include <cstdio>
+#include <vector>
+using namespace std;
+
 typedef int RC;
 typedef unsigned PageNum;
 
@@ -12,15 +16,15 @@ class PF_FileHandle;
 class PF_Manager
 {
 public:
-    static PF_Manager* Instance(int cacheNumPages);                     // Access to the _pf_manager instance
-
+    static PF_Manager* Instance();                                      // Access to the _pf_manager instance
+        
     RC CreateFile    (const char *fileName);                            // Create a new file
     RC DestroyFile   (const char *fileName);                            // Destroy a file
     RC OpenFile      (const char *fileName, PF_FileHandle &fileHandle); // Open a file
-    RC CloseFile     (PF_FileHandle &fileHandle);                       // Close a file
+    RC CloseFile     (PF_FileHandle &fileHandle);                       // Close a file 
 
-protected:
-    PF_Manager(int cacheNumPages);                                      // Constructor
+protected:    
+    PF_Manager();                                                       // Constructor
     ~PF_Manager   ();                                                   // Destructor
 
 private:
@@ -38,6 +42,13 @@ public:
     RC WritePage(PageNum pageNum, const void *data);                    // Write a specific page
     RC AppendPage(const void *data);                                    // Append a specific page
     unsigned GetNumberOfPages();                                        // Get the number of pages in the file
- };
+    RC setFile(FILE* &myFile);											//Set the file to the FileHandle
+    RC closeFile();
 
+private:
+    FILE* myFile;
+    int pageCount;
+
+ };
+ 
  #endif
